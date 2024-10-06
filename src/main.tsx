6ab6329f4030991220b6bb85fcb8347bbdfd3391
kishimotonico <kishimotonico@gmail.com>
2024-10-07 00:29:13 +0900

    Install React Router v6

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
